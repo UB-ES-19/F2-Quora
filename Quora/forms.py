ba79afc8fb060aa1e13b7b47e1db49636bb3cd42
Arnau <arnaugamez@protonmail.com>
2019-09-30 15:23:15 +0200

    Add custom User management models forms.

---
+++ Quora/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import User
+from django.contrib.auth.forms import AuthenticationForm
+
+class RegistrationForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'password'
+        ]
+    
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        print(user)
+
+        if commit:
+            user.save()
+        return user
+
+class LoginForm(AuthenticationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'password'
+        ]
--- Quora/views.py
+++ Quora/views.py
@@ -1,15 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate
-from django.contrib import auth
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User, Group
 
-def index(request):
-    context = {}
+from .forms import RegistrationForm, LoginForm
+
+
+
+def index(request, *args, **kwards):
+    form_reg = RegistrationForm(request.POST)
+    form_login = LoginForm(request.POST)
+
+    if request.method == "POST":
+        # print("debug1")
+
+        if request.POST.get("submit") == "sign_up":
+            
+            # print("debug2")
+            if form_reg.is_valid():
+                # print("debug3")
+                form_reg.save()
+                # redirect somewhere OR show something
+            
+        elif request.POST.get("submit") == "login":
+            pass
+
+    context = {
+        'form_reg': form_reg,
+        'form_login': form_login
+    }
+
     return render(request, 'auth.html', context)
